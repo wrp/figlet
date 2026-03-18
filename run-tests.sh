2f5ae9d909e80557ba36bd87dcb8fb19728e4f59
William Pursell <william.r.pursell@gmail.com>
2026-03-17 19:12:35 -0600

    Add test with empty argument
    Type: tst

--- run-tests.sh
+++ run-tests.sh
@@ -80,6 +80,7 @@ run_test "uskata control file" "printf 'ABCDE'|$CMD -fbanner -Cuskata"
 run_test "jis0201 control file" "printf '\261\262\263\264\265'|$CMD -fbanner -Cjis0201"
 run_test "right-to-left smushing with JavE font" "$cmd -f tests/flowerpower -R"
 run_test "test arguments" "$CMD this   is 'a  test'"
+run_test "test arg with newline" "$CMD this is '' 'a   test'"
 
 rm -f "$OUTPUT"
 
