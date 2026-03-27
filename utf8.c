ec6820644e248f6c80001235300e46a4eacc72a1
William Pursell <william.r.pursell@gmail.com>
2026-03-27 06:42:05 -0600

    Use 'unsigned char' vice u_char
    u_char is a BSD/POSIX-ism, typedef'd in <sys/types.h> on macOS and
    some Linuxes.  It's not in the C standard.
    
    Type: ref

--- utf8.c
+++ utf8.c
@@ -31,7 +31,7 @@
 #define _BOM	0xfeff
 
 static int __wchar_forbitten(wchar_t sym);
-static int __utf8_forbitten(u_char octet);
+static int __utf8_forbitten(unsigned char octet);
 
 static int
 __wchar_forbitten(wchar_t sym)
@@ -45,7 +45,7 @@ __wchar_forbitten(wchar_t sym)
 }
 
 static int
-__utf8_forbitten(u_char octet)
+__utf8_forbitten(unsigned char octet)
 {
 
 	switch (octet) {
@@ -89,7 +89,7 @@ size_t
 utf8_to_wchar(const char *in, size_t insize, wchar_t *out, size_t outsize,
     int flags)
 {
-	u_char *p, *lim;
+	unsigned char *p, *lim;
 	wchar_t *wlim, high;
 	size_t n, total, i, n_bits;
 
@@ -97,7 +97,7 @@ utf8_to_wchar(const char *in, size_t insize, wchar_t *out, size_t outsize,
 		return (0);
 
 	total = 0;
-	p = (u_char *)in;
+	p = (unsigned char *)in;
 	lim = p + insize;
 	wlim = out + outsize;
 
@@ -220,7 +220,7 @@ wchar_to_utf8(const wchar_t *in, size_t insize, char *out, size_t outsize,
     int flags)
 {
 	wchar_t *w, *wlim, ch;
-	u_char *p, *lim, *oc;
+	unsigned char *p, *lim, *oc;
 	size_t total, n;
 
 	if (in == NULL || insize == 0 || (outsize == 0 && out != NULL))
@@ -228,7 +228,7 @@ wchar_to_utf8(const wchar_t *in, size_t insize, char *out, size_t outsize,
 
 	w = (wchar_t *)in;
 	wlim = w + insize;
-	p = (u_char *)out;
+	p = (unsigned char *)out;
 	lim = p + outsize;
 	total = 0;
 	for (; w < wlim; w++) {
@@ -269,7 +269,7 @@ wchar_to_utf8(const wchar_t *in, size_t insize, char *out, size_t outsize,
 
 		/* make it work under different endians */
 		ch = htonl(*w);
-		oc = (u_char *)&ch;
+		oc = (unsigned char *)&ch;
 		switch (n) {
 		case 1:
 			*p = oc[3];
